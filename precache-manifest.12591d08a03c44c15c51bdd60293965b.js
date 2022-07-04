056f372f60896defb8468c0ff9640411af2bdac0
Jay Ambaliya <jaypatel0067@gmail.com>
2022-07-04 13:41:18 -0400

    Updates

---
+++ precache-manifest.12591d08a03c44c15c51bdd60293965b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "70f8a102b8ee009c9d3c",
+    "url": "/Portfolio_Jay_Ambaliya/static/css/main.b7725d96.chunk.css"
+  },
+  {
+    "revision": "70f8a102b8ee009c9d3c",
+    "url": "/Portfolio_Jay_Ambaliya/static/js/main.70f8a102.chunk.js"
+  },
+  {
+    "revision": "69681d1ab596927ed9ed",
+    "url": "/Portfolio_Jay_Ambaliya/static/css/1.e2c5c4ff.chunk.css"
+  },
+  {
+    "revision": "69681d1ab596927ed9ed",
+    "url": "/Portfolio_Jay_Ambaliya/static/js/1.69681d1a.chunk.js"
+  },
+  {
+    "revision": "77e02a81d87285efbe83",
+    "url": "/Portfolio_Jay_Ambaliya/static/js/runtime~main.77e02a81.js"
+  },
+  {
+    "revision": "52c7f265be40073b3db0550572a87cf6",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/Jay Ambaliya_resume.52c7f265.pdf"
+  },
+  {
+    "revision": "8df897ec6ede2408b6370dea2213c638",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/012.8df897ec.jpg"
+  },
+  {
+    "revision": "608931a5983269fb251c2a73cc1beceb",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/media_player.608931a5.png"
+  },
+  {
+    "revision": "6d6818ed5d9426a088c59d4704447fa9",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/portfolio.6d6818ed.png"
+  },
+  {
+    "revision": "3f138f844a4e1fb3c042b75d356f1c5c",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/realtime_chatapp.3f138f84.png"
+  },
+  {
+    "revision": "9d333bf3d1cbd798f27f6397c0076d33",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/Counstruction.9d333bf3.png"
+  },
+  {
+    "revision": "d60c7000287aa055113c3f41c24d1940",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/array_visualizer.d60c7000.png"
+  },
+  {
+    "revision": "40bf2cd3670bf2705978ac1622336171",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/about_img.40bf2cd3.png"
+  },
+  {
+    "revision": "703e0053350a0e84e257f3b82048143c",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/contectbg.703e0053.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Portfolio_Jay_Ambaliya/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "d96552bd6e4c8c9cade1052f68c34047",
+    "url": "/Portfolio_Jay_Ambaliya/index.html"
+  }
+];
